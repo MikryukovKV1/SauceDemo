e1160331373ad7144c4ba5c346d5786b94780470
MikryukovKV1 <keksmkeksm@gmail.com>
2024-06-20 17:29:00 +0400

    locators

---
+++ src/main/java/SaucedemoInventoryPage.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class SaucedemoInventoryPage {
+
+    WebDriver driver = new ChromeDriver();
+
+    WebElement elementId = driver.findElement(By.id("item_4_title_link"));
+    WebElement elementName = driver.findElement(By.name("add-to-cart-sauce-labs-fleece-jacket"));
+    WebElement elementClass = driver.findElement(By.className("shopping_cart_link"));
+    List<WebElement> elementsTag = driver.findElements(By.tagName("a"));
+    WebElement elementText = driver.findElement(By.linkText("Sauce Labs Bike Light"));
+    List<WebElement> elementsPartText = driver.findElements(By.partialLinkText("T-Shirt"));
+    WebElement elementXpAttr = driver.findElement(By.xpath("//a[@data-test=\"item-4-img-link\"]"));
+    WebElement elementXpText = driver.findElement(By.xpath("//div[text()=\"Sauce Labs Onesie\"]"));
+    WebElement elementXpContText = driver.findElement(By.xpath("//div[contains(text(),\"Red\")]"));
+    WebElement elementXpContAttr = driver.findElement(By.xpath("//a[contains(@id,\"about_sidebar\")]"));
+    WebElement elementXpAncestor = driver.findElement(By.xpath("//*[contains(text(),\"Sauce Labs\")]//ancestor::a"));
+    WebElement elementXpPreced = driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]/preceding::div[contains(text(),\"$\")]"));
+    WebElement elementXpFollow = driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-onesie\"]/following::div[contains(text(),\"$\")]"));
+    WebElement elementXpDescen = driver.findElement(By.xpath("//div[@class=\"inventory_item\"]/descendant::a[@id=\"item_3_title_link\"]"));
+}
