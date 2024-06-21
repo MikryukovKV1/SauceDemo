9832b22eb9cf334b79750d3f0b67d60fb225face
Mikryukov_KV <kon.mikryukov@yandex.ru>
2024-06-21 17:15:21 +0400

    Login page test

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import pages.SauceLoginPage;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    WebDriver driver;
+    SauceLoginPage loginPage;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        loginPage = new SauceLoginPage(driver);
+    }
+
+
+    @AfterMethod
+    public void close() {
+        driver.quit();
+    }
+}
